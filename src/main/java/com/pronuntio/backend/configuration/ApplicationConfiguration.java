aa24dd7cc5278a4684b3add78d63ff03cf507619
Igor Karpukhin <i.karpukhin@travelaudience.com>
2018-10-17 10:50:03 +0200

    Initial commit

---
+++ src/main/java/com/pronuntio/backend/configuration/ApplicationConfiguration.java
@@ -0,0 +1,11 @@
+package com.pronuntio.backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@Configuration
+public class ApplicationConfiguration {
+}
